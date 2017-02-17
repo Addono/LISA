b54eaac5d98c986415a10e265939eda9d159e99c
Addono <a.d.knapen@student.tue.nl>
2017-02-17 13:49:27 +0100

    css/refactor: now using a local self-hosted version of font-awsome

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,7 +19,7 @@
     <!--     Fonts and icons     -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?=base_url('resources/css/font-awesome.min.css')?>" />
 
     <!-- CSS Files -->
     <link href="<?=base_url('resources/css/bootstrap.min.css')?>" rel="stylesheet" />
