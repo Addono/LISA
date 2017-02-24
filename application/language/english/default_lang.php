3e4e260bfa25177fe0d0d3dbb9fbd9f6f12035e6
Addono <a.d.knapen@student.tue.nl>
2017-02-24 10:38:04 +0100

    fix: updated language files

--- application/language/english/default_lang.php
+++ application/language/english/default_lang.php
@@ -1,12 +1,8 @@
-<?php
+<?php  defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * @author Adriaan Knapen <a.d.knapen@protonmail.com>
  * @date 5-2-2017
  */
-
-$lang['application_name'] = 'Name placeholder';
-$lang['application_title'] = 'Title placeholder';
-$lang['copyright'] = '2017, Adriaan Knapen';
 
 $lang['navigation_toggle'] = 'Toggle navigation';
 
--- application/language/nederlands/default_lang.php
+++ application/language/nederlands/default_lang.php
@@ -1,12 +1,8 @@
-<?php
+<?php  defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * @author Adriaan Knapen <a.d.knapen@protonmail.com>
  * @date 5-2-2017
  */
-
-$lang['application_name'] = 'Jouw naam hier';
-$lang['application_title'] = 'Jouw titel hier';
-$lang['copyright'] = '2017, Adriaan Knapen';
 
 $lang['navigation_toggle'] = 'Open/sluit menu';
 
