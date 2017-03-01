b048c3b2f02ccdc695f73d2efa8c8a8e295c9c95
Addono <a.d.knapen@student.tue.nl>
2017-03-01 23:32:11 +0100

    new: extended default model functionality

---
+++ application/models/User_LoginRole.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 1-3-2017
+ */
+
+/**
+ * Class User_LoginRole
+ * @property CI_DB_query_builder    $db
+ */
+class User_LoginRole extends ModelFrame
+{
+    protected function dependencies()
+    {
+        return [
+            User::class,
+            Role::class,
+            LoginRole::class,
+        ];
+    }
+
+    public function getUsersWithRole($roleId) {
+        return $this->join()
+            ->where([field(Role::FIELD_ROLE_ID, LoginRole::name()) => $roleId])
+            ->get(LoginRole::name())
+            ->result_array();
+    }
+
+    public function join() {
+        return $this->db
+            ->join(
+                User::name(),
+                eq(
+                    field(Login::FIELD_LOGIN_ID, User::name()),
+                    field(Login::FIELD_LOGIN_ID, LoginRole::name())
+                )
+            );
+    }
+}
