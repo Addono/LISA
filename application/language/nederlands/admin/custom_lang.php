4cc6c428a906a72fa6f52f0b850916eeb5b6340f
Addono <a.d.knapen@student.tue.nl>
2017-03-03 20:35:18 +0100

    new added Dutch translation

---
+++ application/language/nederlands/admin/custom_lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 2-3-2017
+ */
+
+// Menu
+$lang['application_menu_transactions'] = 'Transacties';
+
+// Give user
+$lang['application_give_user_title'] = 'Geef %s nieuwe consumpties';
+$lang['application_give_user_subtitle'] = 'Geef consumpties aan %s';
+
+$lang['application_give_user_amount'] = 'Hoeveelheid';
+$lang['application_give_user_amount_placeholder'] = 'Huidig: %s';
+$lang['application_give_user_amount_help'] = 'De hoeveelheid consumpties de gebruiker ontvangt.';
+
+$lang['application_give_user_success'] = '%s ontving %s consumptie(s).';
+
+$lang['application_give_user_error_amount_required'] = 'De hoeveelheid is verplicht.';
+$lang['application_give_user_error_not_integer'] = 'Ongeldige hoeveelheid opgegeven, alleen hele getallen zijn toegestaan.';
+$lang['application_give_user_error_amount_not_positive'] = 'Alleen positieve hoeveelheden zijn toegestaan.';
+
+$lang['application_give_user_submit'] = 'Geef';
+
+$lang['application_user_overview_tooltip_give_user'] = 'Consumpties toevoegen';
+
+// Transactions
+$lang['application_transactions_title'] = 'Transactie overzicht';
+$lang['application_transactions_table_title'] = 'Alle transacties';
+
+$lang['application_transactions_table_header_author'] = 'Auteur';
+$lang['application_transactions_table_header_subject'] = 'Gebruiker';
+$lang['application_transactions_table_header_amount'] = 'Nieuwe hoeveelheid';
+$lang['application_transactions_table_header_delta'] = 'Verschil';
+$lang['application_transactions_table_header_time'] = 'Tijd';
+
+//@Overwrite
+$lang['application_name'] = 'Lisa Backend';
+$lang['application_title'] = 'Lisa Backend';
+
+$lang['application_dashboard_body'] = 'Eens zal er hier misschien widgets staan, maar voor nu staat hier alleen een korte introductie. Je kan nieuwe gebruikers toevoegen onder Beheer Gebruikers > Gebruiker toevoegen.<p> Om de gegevens van alle gebruikers te zien en aan te passen ga naar Beheer Gebruikers > Overzicht, hier kan je ook nieuwe consumpties aan gebruikers toekennen.<b>N.B.:</b> Alleen gebruikers met de <i>User</i>-rol kunnen consumpties uitgeven.';
---
+++ application/language/nederlands/default/custom_lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 1-3-2017
+ */
+$lang['consume_title'] = 'Consume!';
+
+$lang['consume_table_head_name'] = 'Name';
+$lang['consume_table_head_credit'] = 'Credit';
+$lang['consume_table_head_consumptions'] = '#Consumptions';
+
+$lang['consume_form_invalid_amount'] = 'Something went wrong with the given amounts, therefore the transaction is aborted. Please try again, if the problem persists please contact the system administrator.';
+$lang['consume_form_invalid_user'] = 'An invalid user has been selected, therefore the transaction is aborted. Please try again, if the problem persist please contact the system administrator.';
+$lang['consume_form_user_success'] = 'Successfully purchased %s consumption(s) for %s.';
+$lang['consume_form_user_failure'] = 'Failed purchasing %s consumptions for %s. Please try again, if the problem persists please contact the system administrator.';
+
+// Transactions
+$lang['transactions_title'] = 'My transactions';
+
+$lang['transactions_table_header_author'] = 'Author';
+$lang['transactions_table_header_subject'] = 'Subject';
+$lang['transactions_table_header_amount'] = 'New amount';
+$lang['transactions_table_header_delta'] = 'Difference';
+$lang['transactions_table_header_time'] = 'Time';
+
+// @Overwrite
+$lang['application_name'] = 'Lisa';
+$lang['application_title'] = 'Lisa is super awesome';
+$lang['copyright'] = '2017, Adriaan Knapen';
+$lang['application_version'] = 'Core v0.2.1-beta';
+
+$lang['default_page_header_title'] = 'Welcome';
+$lang['default_page_header_body'] = 'This is Lisa, login to get started!';
