462de10fa1af3944ff46921e239b0206ae8df47e
Addono <a.d.knapen@student.tue.nl>
2017-04-23 14:15:18 +0200

    update: new version from upstream

---
+++ application/pages/default/ApiBuyPage.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 5-4-2017
+ */
+
+/**
+ * Class ApiBuyPage
+ */
+class ApiBuyPage extends ApiFrame
+{
+
+    function call()
+    {
+        $id = set_value('id');
+
+        if ($id === '' || ! is_numeric($id)) {
+            $this->setResult('error', 'invalidArgument');
+        } else {
+            $id = set_value('id');
+
+            $authorId = getLoggedInLoginId($this->ci->session);
+            $this->ci->Consumption->change($id, $authorId, -1);
+        }
+    }
+
+    function hasAccess(): bool
+    {
+        return isLoggedInAndHasRole($this->ci, [Role::ROLE_USER]);
+    }
+
+    /**
+     * Defines which models should be loaded.
+     *
+     * @return array;
+     */
+    protected function getModels(): array
+    {
+        return [
+            Login::class,
+            Consumption::class,
+            User::class,
+            Role::class,
+            LoginRole::class,
+            User_Consumption_LoginRole::class,
+        ];
+    }
+
+    /**
+     * Defines which libraries should be loaded.
+     *
+     * @return array;
+     */
+    protected function getLibraries(): array
+    {
+        return [];
+    }
+
+    /**
+     * Defines which helpers should be loaded.
+     *
+     * @return array;
+     */
+    protected function getHelpers(): array
+    {
+        return [];
+    }
+}
---
+++ application/pages/default/TestPage.php
@@ -0,0 +1,92 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 6-2-2017
+ */
+
+class TestPage extends PageFrame
+{
+
+    public function getViews(): array
+    {
+        return [
+            'test-header',
+            'intersection',
+        ];
+    }
+
+    public function hasAccess(): bool
+    {
+        return true;
+    }
+
+    protected function getFormValidationRules()
+    {
+        return false;
+    }
+
+    /**
+     * Function which is called after construction and before the views are rendered.
+     */
+    public function beforeView()
+    {
+    ob_start();
+    ?><script>
+    $(document).ready(function() {
+        $('.buy').onclick(function () {
+            $.ajax({
+                url: "<?=site_url($this->data['group'] . '/ApiBuy')?>",
+                data: {
+                    id: 23
+                },
+                type: "POST",
+                dataType: "json"
+            });
+        })
+            .done(function (json) {
+                console.log(json);
+            })
+            .fail(function (xhr, status, errorMessage) {
+                alert(errorMessage);
+            });
+    });
+    </script><?php
+        $this->addScript(ob_get_contents());
+        ob_clean();
+        $this->addScript('abc');
+    }
+
+    /**
+     * Defines which models should be loaded.
+     *
+     * @return array;
+     */
+    protected function getModels(): array
+    {
+        return [
+            Role::class,
+        ];
+    }
+
+    /**
+     * Defines which libraries should be loaded.
+     *
+     * @return array;
+     */
+    protected function getLibraries(): array
+    {
+        return [];
+    }
+
+    /**
+     * Defines which helpers should be loaded.
+     *
+     * @return array;
+     */
+    protected function getHelpers(): array
+    {
+        return [];
+    }
+}
