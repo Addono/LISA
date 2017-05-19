0e2415d5552016be52d52f80c4e224322fe64655
Adriaan Knapen <a.d.knapen@protonmail.com>
2017-05-19 21:23:41 +0200

    fix: possible routing problems

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['install'] = 'Install/index';
+$route['Install'] = 'Install/index';
 $route['(:any)'] = 'Handler/index/$1';
 $route['(:any)/(:any)'] = 'Handler/index/$1/$2';
 $route['(:any)/(:any)/(:any)'] = 'Handler/index/$1/$2/$3';
