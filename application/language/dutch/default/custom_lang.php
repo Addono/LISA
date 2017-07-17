17f4f4564c65801e89ba2e7734ea41ce75a02207
Adriaan Knapen <a.d.knapen@protonmail.com>
2017-07-17 14:08:52 +0800

    change: increased version number

--- application/language/dutch/default/custom_lang.php
+++ application/language/dutch/default/custom_lang.php
@@ -55,7 +55,7 @@ $lang['footer_source'] = 'Broncode';
 $lang['application_name'] = 'Lisa';
 $lang['application_title'] = 'Lisa is super awesome';
 $lang['copyright'] = '2017, Adriaan Knapen';
-$lang['application_version'] = 'Core v1.1';
+$lang['application_version'] = 'Core v1.2.1';
 
 $lang['default_page_header_title'] = 'Hoi ik ben Lisa!';
 $lang['default_page_header_body'] = 'Log in om te beginnen';
