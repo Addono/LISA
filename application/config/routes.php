7933504a3434005a7383f48ec2c42d84befae10d
Addono <a.d.knapen@protonmail.com>
2017-10-07 21:44:53 +0200

    fix: Added extra install route

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['Install'] = 'Install/index';
 $route['install'] = 'Install/index';
 $route['(:any)'] = 'Handler/index/$1';
 $route['(:any)/(:any)'] = 'Handler/index/$1/$2';
