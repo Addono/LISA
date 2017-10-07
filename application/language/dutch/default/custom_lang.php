4b142f6f82a551c90839b9cfa0e5dfba94c1defe
Addono <a.d.knapen@protonmail.com>
2017-10-07 21:30:06 +0200

    refactor: incremented minor version

--- application/language/dutch/default/custom_lang.php
+++ application/language/dutch/default/custom_lang.php
@@ -58,7 +58,7 @@ $lang['footer_source'] = 'Broncode';
 $lang['application_name'] = 'Lisa';
 $lang['application_title'] = 'Lisa is super awesome';
 $lang['copyright'] = '2017, Adriaan Knapen';
-$lang['application_version'] = 'Core v1.2.1';
+$lang['application_version'] = 'Core v1.3.0';
 
 $lang['default_page_header_title'] = 'Hoi ik ben Lisa!';
 $lang['default_page_header_body'] = 'Log in om te beginnen';
--- application/language/english/default/custom_lang.php
+++ application/language/english/default/custom_lang.php
@@ -58,7 +58,7 @@ $lang['footer_source'] = 'Source';
 $lang['application_name'] = 'Lisa';
 $lang['application_title'] = 'Lisa is super awesome';
 $lang['copyright'] = '2017, Adriaan Knapen';
-$lang['application_version'] = 'Core v1.2.1';
+$lang['application_version'] = 'Core v1.3.0';
 
 $lang['default_page_header_title'] = 'Hello, I am Lisa!';
 $lang['default_page_header_body'] = 'Login to get started';
