77eb51922a58ec4310bd2d86b127d4f5c0c68469
Addono <a.d.knapen@protonmail.com>
2017-10-08 13:19:29 +0200

    new: added leaderboard functionality

--- application/models/Transaction.php
+++ application/models/Transaction.php
@@ -57,6 +57,42 @@ class Transaction extends ModelFrame
             ->result_array();
     }
 
+    public function getCountForSubject($subjectId) {
+        return $this->db
+            ->where([self::FIELD_SUBJECT_ID => $subjectId])
+            ->count_all(self::name());
+    }
+
+    /**
+     * Checks if a subject user is on the leaderboard of the sum of most positive or negative transactions.
+     *
+     * @param int $subjectId The login ID of the subject.
+     * @param bool $positive True if transactions with only positive deltas should regarded, false for only negative
+     *      deltas.
+     * @param int $position The amount of spots on the leaderboard.
+     * @return bool True if the user is on the leaderboard, false otherwise.
+     */
+    public function getSumDeltaSubjectIdWithinTop(int $subjectId, bool $positive, int $position): bool {
+        // Retrieve the sum of the delta of all negative or positive transactions, ordered and limited to retrieve only highest values.
+        $sumQuery = $this->db
+            ->select(self::FIELD_SUBJECT_ID)
+            ->select_sum(self::FIELD_DELTA, 'sum')
+            ->where(self::FIELD_DELTA . ($positive?'>':'<') . ' 0')
+            ->group_by(self::FIELD_SUBJECT_ID)
+            ->limit($position)
+            ->order_by('sum ' . ($positive?'DESC':'ASC'))
+            ->get_compiled_select(self::name());
+
+        // Check if the specified subject is within the earlier retrieved leaderboard.
+        $result = $this->db
+            ->from('(' . $sumQuery . ') `t`')
+            ->where(['t.' . self::FIELD_SUBJECT_ID => $subjectId])
+            ->get();
+
+        // If a row is returned, then the user is on the leaderboard.
+        return $result->num_rows() > 0;
+    }
+
     public function v1() {
         return [
             'requires' => [
---
+++ application/views/page/default/leaderboard-header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Adriaan Knapen <a.d.knapen@protonmail.com>
+ * @date 1-3-2017
+ */
+?>
+
+<div class="col-md-12 vmargin col-md-offset-0">
+    <div class="container">
+        <!-- Tabs with icons on Card -->
+        <div class="card card-nav-tabs">
+            <div class="header header-primary">
+                <div class="text-center">
+                    <h4><?=lang('leaderboard_title')?></h4>
+                    <p><?=lang('leaderboard_subtext')?></p>
+                </div>
+            </div>
+            <div class="content">
+                <div class="content">
+                    <div class="tab-content text-center"><div class="tab-pane active" id="ordered-first-name">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th><?=lang('leaderboard_table_header_name')?></th>
+                                <th><?=lang('leaderboard_table_header_sum')?></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($entries as $entry) { ?>
+                                <tr>
+                                    <td><?=$entry[$fields['first_name']] . ' ' . $entry[$fields['last_name']]?></td>
+                                    <td><?=-$entry[$fields['sum']]?></td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
