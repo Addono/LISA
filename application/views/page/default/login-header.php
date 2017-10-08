8361a11bd5fb5451d9f43ab8fb563dbe2cf3e546
Addono <a.d.knapen@protonmail.com>
2017-10-08 14:18:28 +0200

    fix: styling issue in login form

--- application/views/page/default/login-header.php
+++ application/views/page/default/login-header.php
@@ -5,11 +5,12 @@
  */
 ?>
 <div class="col-md-6 vmargin">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-                <div class="card card-signup">
-                    <?=form_open()?>
+    <div class="col-md-6 vmargin">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                    <div class="card card-signup">
+                        <?=form_open()?>
                         <div class="header header-primary text-center">
                             <h4><?=lang('login_login')?></h4>
                         </div>
@@ -17,9 +18,9 @@
                         <div class="content">
 
                             <div class="input-group form-group label-floating">
-                                <span class="input-group-addon">
-                                    <i class="fa fa-user fa-lg"></i>
-                                </span>
+                            <span class="input-group-addon">
+                                <i class="fa fa-user fa-lg"></i>
+                            </span>
                                 <label class="control-label"><?=lang('login_username')?></label>
                                 <input type="text" name="username" class="form-control" value="<?=set_value('username')?>" autofocus />
                             </div>
@@ -32,11 +33,12 @@
                                 <input type="password" name="password" class="form-control" />
                             </div>
                         </div>
+                        <div class="footer text-center">
+                            <input type="submit" value="<?=lang('login_submit')?>" class="btn btn-primary btn-lg">
+                        </div>
+                        <?=form_close()?>
                     </div>
-                    <div class="footer text-center">
-                        <input type="submit" value="<?=lang('login_submit')?>" class="btn btn-primary btn-lg">
-                    </div>
-                <?=form_close()?>
+                </div>
             </div>
         </div>
     </div>
