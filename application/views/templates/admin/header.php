3366f53fa4500ba13485d5aad6cec294e2ffb472
Addono <a.d.knapen@protonmail.com>
2017-10-17 12:29:49 +0200

    refactor: removed piwik code

--- application/views/templates/admin/header.php
+++ application/views/templates/admin/header.php
@@ -33,21 +33,6 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <!-- Piwik -->
-    <script type="text/javascript">
-        var _paq = _paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="//bar.sdhd.eu/piwik/";
-            _paq.push(['setTrackerUrl', u+'piwik.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-        })();
-    </script>
-    <!-- End Piwik Code -->
 </head>
 
 <body>
--- application/views/templates/default/header.php
+++ application/views/templates/default/header.php
@@ -37,21 +37,6 @@
     <link href="<?=base_url('resources/css/material-kit.css')?>" rel="stylesheet"/>
     <link href="<?=base_url('resources/css/style.css')?>" rel="stylesheet" />
 
-    <!-- Piwik -->
-    <script type="text/javascript">
-        var _paq = _paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="//bar.sdhd.eu/piwik/";
-            _paq.push(['setTrackerUrl', u+'piwik.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-        })();
-    </script>
-    <!-- End Piwik Code -->
 </head>
 
 <body style="background-image: url('<?=base_url('resources/img/bg.jpeg')?>')">
