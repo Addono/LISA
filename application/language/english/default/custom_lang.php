74c02f02099347ac7405e565dcd427720b9ac355
Adriaan Knapen <hi@aknapen.nl>
2020-03-26 14:55:13 +0100

    🔖 Bump version to 2.0.4

--- application/language/english/default/custom_lang.php
+++ application/language/english/default/custom_lang.php
@@ -82,7 +82,7 @@ $lang['footer_source'] = 'Source';
 // @Overwrite
 $lang['application_name'] = 'Lisa';
 $lang['application_title'] = 'Lisa is super awesome';
-$lang['application_version'] = 'v2.0.3';
+$lang['application_version'] = 'v2.0.4';
 
 $lang['default_page_header_title'] = 'Hello, I am Lisa!';
 $lang['default_page_header_body'] = 'Login to get started';
